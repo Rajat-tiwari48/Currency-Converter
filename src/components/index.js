d2f213077238080716d338c0827c9d0bd452054d
Rajat-tiwari48 <115857854+Rajat-tiwari48@users.noreply.github.com>
2024-07-04 15:30:10 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import InputBox from "./InputBox";
+
+
+//we use this because all components are imported here and all are exported with ite same with use of , so it
+//decrease the complexity of the program and easy the understand 
+// because there will be a single file through which all componrnts can be accessed
+// and for accessing also we need to import only one file and can access all components
+export{InputBox}
